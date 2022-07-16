d3fb0b860caad9cc2c91f054f29b37e0168b7c59
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-07-16 17:01:30 -0400

    modify url endpoint

--- config/app.js
+++ config/app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/advertisement',AddRouter);
+app.use('/advertisements', AddRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
