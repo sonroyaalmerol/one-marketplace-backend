a2478a81b2bebe1e6b324ad932f11595ff887add
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-07-16 14:47:16 -0400

    comment out users routes

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// var usersRouter = require('./routes/users');
 var AddRouter = require('./routes/advertisement');
 
 var dbConfig = require('./config/db');
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
 app.use('/advertisement',AddRouter);
 
 // catch 404 and forward to error handler
