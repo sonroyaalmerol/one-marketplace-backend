a856f5ef0e516fc859fb5a09e977a4fce8456104
Son Roy Almerol <sonroya.almerol@gmail.com>
2022-07-23 14:50:07 -0400

    fix user require typo

--- config/app.js
+++ config/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 
 const indexRouter = require('../routes/index');
-const usersRouter = require('../routes/users');
+const usersRouter = require('../routes/user');
 const advertisementRouter = require('../routes/advertisement');
 
 const dbConfig = require('./db');
